136bd2ff64bd718a2714bf0a46c63f7b0847d862
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-19 00:25:00 -0700

    javascript for expanding details, more styles

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,26 @@
 	"use strict";
 
 	// Add Scripts Here
+
+	function showStats(event) {
+		//Get the menu & detail button text elements
+		var statsMenu = document.getElementById("expanded-stats")
+		var detailbuttonText = detailButton.getElementsByTagName("span")
+		
+		//toggle hidden class and update expand text
+		if(statsMenu.classList.contains("hidden")) {
+			statsMenu.classList.remove("hidden")
+			detailbuttonText[0].textContent = "Hide Details"
+		} else {
+			statsMenu.classList.add("hidden")
+			detailbuttonText[0].textContent = "Show Details"
+			
+		}
+
+	}
+
+	//Expand Details Event Listeiner
+	var detailButton = document.getElementById("detail-button")
+	detailButton.addEventListener('click', showStats, false)
     	
 })();
