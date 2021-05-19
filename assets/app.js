e2fc2474d23b78c6f7d7eefc9c7f8b697d6a4795
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-19 14:36:28 -0700

    added js for toggling mobile menu, updates arrow icons

--- assets/app.js
+++ assets/app.js
@@ -7,21 +7,42 @@
 		//Get the menu & detail button text elements
 		var statsMenu = document.getElementById("expanded-stats")
 		var detailbuttonText = detailButton.getElementsByTagName("span")
+		var detailButtonArrow = detailButton.getElementsByTagName("i")
 		
-		//toggle hidden class and update expand text
+		statsMenu.classList.toggle("hidden")
+		detailButtonArrow[0].classList.toggle("fa-angle-down")
+		detailButtonArrow[0].classList.toggle("fa-angle-up")
+		
+		//update expand text
 		if(statsMenu.classList.contains("hidden")) {
-			statsMenu.classList.remove("hidden")
 			detailbuttonText[0].textContent = "Hide Details"
 		} else {
-			statsMenu.classList.add("hidden")
-			detailbuttonText[0].textContent = "Show Details"
 			
+			detailbuttonText[0].textContent = "Show Details"
 		}
+
+	}
+
+
+	function showMobileMenu(event) {
+		//get relevant elements
+		var mobileMenu = document.getElementById("mobile-menu")
+		var mobileMenuIcon = mobileMenu.getElementsByTagName("i")
+		var mobileMenuExpand = mobileMenu.getElementsByTagName("nav")
+
+		//Toggle classes for hidden and arrow icon
+		mobileMenuIcon[0].classList.toggle("fa-angle-down")
+		mobileMenuIcon[0].classList.toggle("fa-angle-up")
+		mobileMenuExpand[0].classList.toggle("hidden")
+
 
 	}
 
 	//Expand Details Event Listeiner
 	var detailButton = document.getElementById("detail-button")
 	detailButton.addEventListener('click', showStats, false)
+
+	var mobileMenuButton = document.getElementById("mobile-menu-button")
+	mobileMenuButton.addEventListener('click', showMobileMenu, false)
     	
 })();
